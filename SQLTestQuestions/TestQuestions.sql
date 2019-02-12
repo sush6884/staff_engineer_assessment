c6301899aae0fc7243961748a0405f04dd1f4f55
Bridget Landis <blandis@priviahealth.com>
2019-02-12 11:06:45 -0500

    staff engineer assessment
    python and sql

---
+++ SQLTestQuestions/TestQuestions.sql
@@ -0,0 +1,176 @@
+USE PERSONDATABASE
+
+/*********************
+Hello! 
+
+Please use the test data provided in the file 'PersonDatabase' to answer the following
+questions. Please also import the dbo.Contracts flat file to a table for use. 
+
+All answers should be written in SQL. 
+
+
+***********************
+
+QUESTION 1
+
+
+The table dbo.Person contains basic demographic information. The source system users 
+input nicknames as strings inside parenthesis. Write a query or group of queries to 
+return the full name and nickname of each person. The nickname should contain only letters 
+or be blank if no nickname exists.
+
+**********************/
+
+
+
+
+
+
+
+
+
+/**********************
+
+QUESTION 2
+
+
+The dbo.Risk table contains risk and risk level data for persons over time for various 
+payers. Write a query that returns patient name and their current risk level. 
+For patients with multiple current risk levels return only one level so that Gold > Silver > Bronze.
+
+
+**********************/
+
+ 
+
+
+
+
+
+/**********************
+
+QUESTION 3
+
+Create a patient matching stored procedure that accepts (first name, last name, dob and sex) as parameters and 
+and calculates a match score from the Person table based on the parameters given. If the parameters do not match the existing 
+data exactly, create a partial match check using the weights below to assign partial credit for each. Return PatientIDs and the
+ calculated match score. Feel free to modify or create any objects necessary in PersonDatabase.  
+
+FirstName 
+	Full Credit = 1
+	Partial Credit = .5
+
+LastName 
+	Full Credit = .8
+	Partial Credit = .4
+
+Dob 
+	Full Credit = .75
+	Partial Credit = .3
+
+Sex 
+	Full Credit = .6
+	Partial Credit = .25
+
+
+**********************/
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************
+
+QUESTION 4
+
+A. Looking at the script 'PersonDatabase', what change(s) to the tables could be made to improve the database structure?  
+
+B. What method(s) could we use to standardize the data allowed in dbo.Person (Sex) to only allow 'Male' or 'Female'?
+
+C. Assuming these tables will grow very large, what other database tools/objects could we use to ensure they remain
+efficient when queried?
+
+
+**********************/
+
+
+
+
+
+
+
+
+
+
+
+/**********************
+
+QUESTION 5
+
+Write a query to return risk data for all patients, all contracts and a moving average of risk for that patient and contract 
+in dbo.Risk. 
+
+**********************/
+
+
+
+
+
+
+
+
+
+
+
+/**********************
+
+QUESTION 6
+
+Write script to load the dbo.Dates table with all applicable data elements for dates 
+between 1/1/2010 and 500 days past the current date.
+
+
+**********************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************
+
+QUESTION 7
+
+Please import the data from the flat file dbo.Contracts.txt to a table to complete this question. 
+
+Using the data in dbo.Contracts, create a query that returns 
+
+	(PersonID, AttributionStartDate, AttributionEndDate) 
+
+The data should be structured so that rows with contiguous ranges are merged into a single row. Rows that contain a 
+break in time of 1 day or more should be entered as a new record in the output. Restarting a row for a new 
+month or year is not necessary.
+
+Use the dbo.Dates table if helpful.
+
+**********************/
+
+
+
+
+
+
